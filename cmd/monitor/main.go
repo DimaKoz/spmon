9925df2ab74c575585173c894707612fe88f1760
DimaKoz <kozhevin.dima@gmail.com>
2023-12-25 13:42:24 +0300

    github actions(tests)

--- cmd/monitor/main.go
+++ cmd/monitor/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"github.com/DimaKoz/spmon/internal/repository"
 	"log"
 )
 
 func main() {
 	log.Println("Hello world")
+
+	a, err := repository.GetHs("")
+	log.Println(err, a)
 }
