701db23cb099fafee338b50cc692f23caba2a4c9
DimaKoz <kozhevin.dima@gmail.com>
2024-01-03 21:43:02 +0300

    tests

--- internal/model/article/articles_internal_test.go
+++ internal/model/article/articles_internal_test.go
@@ -7,10 +7,14 @@ import (
 )
 
 //nolint:exhaustruct
+var (
+	testMedia0    = Media{ID: "some_id"}
+	testMedia1    = Media{ID: "some_id_1"}
+	testMediaNoID = Media{}
+)
+
+//nolint:exhaustruct
 func TestHeadlineGetMedia(t *testing.T) {
-	media0 := Media{ID: "some_id"}
-	media1 := Media{ID: "some_id_1"}
-	mediaNoID := Media{}
 	tests := []struct {
 		name     string
 		headline Headline
@@ -19,14 +23,14 @@ func TestHeadlineGetMedia(t *testing.T) {
 		{
 			name: "has media",
 			headline: Headline{
-				Cover:   media0,
-				Authors: []Author{{Avatar: media1}, {Avatar: mediaNoID}},
+				Cover:   testMedia0,
+				Authors: []Author{{Avatar: testMedia1}, {Avatar: testMediaNoID}},
 			},
-			want: []Media{media0, media1},
+			want: []Media{testMedia0, testMedia1},
 		},
 		{
 			name:     "no media",
-			headline: Headline{Cover: mediaNoID},
+			headline: Headline{Cover: testMediaNoID},
 			want:     []Media{},
 		},
 	}
@@ -35,6 +39,26 @@ func TestHeadlineGetMedia(t *testing.T) {
 		t.Run(tUnit.name, func(t *testing.T) {
 			got := tUnit.headline.getMedia()
 			assert.Equal(t, tUnit.want, got)
+		})
+	}
+}
+
+func TestGetMediaFromSliceMedia(t *testing.T) {
+	tests := []struct {
+		name  string
+		media []Media
+		want  []Media
+	}{
+		{
+			name:  "all_cases",
+			media: []Media{testMedia0, testMediaNoID, testMedia1},
+			want:  []Media{testMedia0, testMedia1},
+		},
+	}
+	for _, tCase := range tests {
+		tUnit := tCase
+		t.Run(tUnit.name, func(t *testing.T) {
+			assert.Equal(t, tUnit.want, getMedia(tUnit.media), "getMedia(%v)", tUnit.media)
 		})
 	}
 }
